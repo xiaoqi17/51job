5d97b57722de0bf92a8afc8e0aa7400dca851153
xiaoqi17 <ftkhg@163.com>
2017-08-10 10:44:55 +0800

    Update 51spider.py

--- 51spider.py
+++ 51spider.py
@@ -7,16 +7,28 @@ import re
 import time
 import json
 import sys
-from requests import HTTPError
+from requests import HTTPError, ConnectionError
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 client = pymongo.MongoClient('localhost', connect=False)
 db = client['51job']
-
-def index_html(headers,i):
+def page_html(headers,urls):
     try:
-        url = 'http://search.51job.com/list/030200%252C00,000000,0000,00,9,99,python,2,{}.html'.format(str(i))
+        r = requests.get(urls, headers=headers)
+        time.sleep(5)
+        r.raise_for_status()  # 检测状态码
+        r.encoding = r.apparent_encoding  # 编码改为UTF-8
+        pages = re.compile('<span class="td">.*?(\d+).*?</span><input id="jump_page"',re.S)
+        page = pages.findall(r.text)[0]
+        return page
+    except ConnectionError:
+        print('Error occurred')
+
+def index_html(headers,url):
+    try:
+
         r = requests.get(url, headers=headers)
         time.sleep(5)
         r.raise_for_status()  # 检测状态码
@@ -30,8 +42,8 @@ def index_html(headers,i):
                 pass
             else:
                 yield urls
-    except:
-        pass
+    except ConnectionError:
+        print('Error occurred')
 
 def page_one_page(url, headers):
     try:
@@ -49,7 +61,7 @@ def page_one_page(url, headers):
                 text = re.sub('\r\n\t\t\t\t\t\t','',text)
                 com = re.compile(
                     r'div class="cn">.*?<h1 title="(.*?)".*?<span class="lname">(.*?)</span>.*?<strong>(.*?)</strong>.*?<p class="cname">.*?title="(.*?)".*?</p>' \
-                    '.*?<div class="t1">.*?<em class="i3"></em>(.*?)</span>.*?<em class="i4"></em>(.*?)</span>' \
+                    '.*?<div class="t1">.*?<em class="i3"></em>(.*?)</span>.*?<em class="i4"></em>(\d+-\d+).*?</span>' \
                     '.*?<span class="label">.*?</span>(.*?)<div class="mt10">', re.S)  # 正则
                 items = re.findall(com, text)
                 for i in items:
@@ -80,18 +92,23 @@ def save_to_mongo(datail):
         return True
     return False
 
-def main(i):
+def main():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '}
-    urls = index_html(headers,i)
-    for url in urls:
-        data = page_one_page(url, headers)
-        # write_to_file(data)
-        if data:save_to_mongo(data)
+    urls = 'http://search.51job.com/list/030200%252C00,000000,0000,00,9,99,python,2,1.html'
+    page = page_html(headers, urls)
+    for i in range(1, int(page) + 1):
+        urls = 'http://search.51job.com/list/030200%252C00,000000,0000,00,9,99,python,2,{}.html'.format(str(i))
+        url = index_html(headers,urls)
+        for url in url:
+            data = page_one_page(url, headers)
+            # write_to_file(data)
+            if data:save_to_mongo(data)
 
 if __name__ == '__main__':
-    pool = Pool()
-    pool.map(main, [i for i in range(1, 44)])
-    pool.close()
-    pool.join()
+    # pool = Pool()
+    # pool.map(main, [i for i in range(1, 44)])
+    # pool.close()
+    # pool.join()
+    main()
 
 
